e163e079d9070bacaad3e6db39a47e8e4212de4e
Sean <sean.daxxtrias@gmail.com>
2025-04-14 18:24:31 -0500

    more baseline porting

---
+++ Mod/Cheats/GameMods.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+//using Mod.Utils;
+using Mod.Game;
+using System.Runtime.Serialization;
+using ObjectManager = Mod.Game.ObjectManager;
+using Il2Cpp;
+using MelonLoader;
+
+namespace Mod.Cheats
+{
+    internal static class GameMods
+    {
+
+    }
+}
---
+++ Mod/Cheats/Patches.cs
@@ -0,0 +1,29 @@
+﻿using Il2CppInterop.Runtime;
+using System.Runtime.Serialization;
+using UnityEngine;
+using Mod.Game;
+//using static UnityEngine.Physics;
+using UnityEngine.UI;
+using Harmony;
+using HarmonyLib;
+using Il2Cpp;
+using UnityEngine.Playables;
+using ObjectManager = Mod.Game.ObjectManager;
+using HarmonyPatch = HarmonyLib.HarmonyPatch;
+using MelonLoader;
+using HarmonyPostfix = HarmonyLib.HarmonyPostfix;
+using HarmonyPrefix = HarmonyLib.HarmonyPrefix;
+using System.Reflection;
+using AccessTools = HarmonyLib.AccessTools;
+//using Il2CppPantheonPersist;
+//using static Il2Cpp.UIChat;
+using Il2CppTMPro;
+
+namespace Mod.Cheats.Patches
+{
+    [HarmonyPatch]
+    internal class HarmonyPatches
+    {
+
+    }
+}
