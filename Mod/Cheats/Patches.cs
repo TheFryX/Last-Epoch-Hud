68c9d6947e7753d89a906c630966286c682ad517
Sean <sean.daxxtrias@gmail.com>
2025-04-14 19:14:53 -0500

    add todo notes about bug report and crash report hooks to mitigate self-report issues

--- Mod/Cheats/Patches.cs
+++ Mod/Cheats/Patches.cs
@@ -15,7 +15,6 @@ using HarmonyPostfix = HarmonyLib.HarmonyPostfix;
 using HarmonyPrefix = HarmonyLib.HarmonyPrefix;
 using System.Reflection;
 using AccessTools = HarmonyLib.AccessTools;
-//using Il2CppPantheonPersist;
 //using static Il2Cpp.UIChat;
 using Il2CppTMPro;
 
@@ -24,6 +23,18 @@ namespace Mod.Cheats.Patches
     [HarmonyPatch]
     internal class HarmonyPatches
     {
+        // todo implement patches to guard accident accidental self reports of modifying the game
+        // patch out the bug report button, and for redundancy the SendBugToServer() function
+        // il2Cppscripts->il2cpp->BugReportWriter->SendBugToServer
+        // il2CppLE->il2Cpp->UIBase->OpenBugReportPanel()
+        // il2CppLE->il2Cpp->CharacterSelectPanelUI->OpenBugReport()
+        // il2CppLE->il2Cpp->Il2CppLE.Telemetry->ClientSessionAnalytics->BugReported()
+        // il2CppLE->il2Cpp->Il2CppLE.Telemetry->ClientSessionAnalytics->OnBugReported()
+        // il2CppLE->il2Cpp->Il2CppLE.Telemetry->ISessionAnalytics->OnBugReported()
+        // il2CppLE->il2Cpp->il2CppLELE.Utility->ClickUp->SubmitNewBugReport()
 
+        // probably should patch out the crash reporter just incase
+        // UnityEngine.CrashReportingModule->UnityEngine.CrashReportHandler->CrashReportHandler()
+        // UnityEngine.CoreModule->UnityEngine->CrashReport()
     }
 }
