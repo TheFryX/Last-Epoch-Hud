18afd981953afc9d2b2aa984c845f966a82f7a84
Sean <sean.daxxtrias@gmail.com>
2025-04-21 03:21:21 -0500

    remove redundant call to fog patcher. less relevant now that we can just toggle it to occur on demand

--- Mod/Mod.cs
+++ Mod/Mod.cs
@@ -40,7 +40,6 @@ namespace Mod
         public override void OnSceneWasLoaded(int buildindex, string sceneName) // Runs when a Scene has Loaded and is passed the Scene's Build Index and Name.
         {
             //MelonLogger.Msg("OnSceneWasLoaded: " + buildindex.ToString() + " | " + sceneName); // occurs before scene init
-            GameMods.FogRemover(true); // to see if this is enough to check for it
         }
 
         public override void OnSceneWasInitialized(int buildindex, string sceneName) // Runs when a Scene has Initialized and is passed the Scene's Build Index and Name.
